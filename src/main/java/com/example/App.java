97d581c7015cd6516a6a7940541b3a8f1ef9ff93
Kobi Biton <kobi.biton@gmail.com>
2014-10-13 00:21:09 +0200

    hello

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World TomTom! Are you a tiered guys? Coffee Break32 ??" );
+        System.out.println( "Hello World How Are You?" );
     }
 }
