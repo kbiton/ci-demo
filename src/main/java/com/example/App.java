68ac2bc51426f639712ed25470b7769c72dea93d
Kobi Biton <kobi.biton@gmail.com>
2014-10-21 10:31:57 +0100

    kobi

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World How Are You???" );
+        System.out.println( "Hey Guys , lets deploy" );
     }
 }
