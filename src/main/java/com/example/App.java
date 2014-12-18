945ad46705af8af891a7126f3956f9a8809f2402
Kobi Biton <kobi.biton@gmail.com>
2014-12-18 13:57:05 +0100

    cool class today

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hey Guys, Coffee? After L ? Of course...!" );
+        System.out.println( "Hey Guys,Wake Up !!! In eed you!~!!" );
     }
 }
